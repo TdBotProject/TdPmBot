f25a295650a32116152ff749c5fb3d90ca5895bf
世界 <i@nekox.me>
2021-01-21 19:13:45 +0800

    Add revoke all

--- src/main/java/io/nekohasekai/pm/manage/AdminCommands.kt
+++ src/main/java/io/nekohasekai/pm/manage/AdminCommands.kt
@@ -6,18 +6,20 @@ import io.nekohasekai.pm.launcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import td.TdApi
+import java.io.File
 
 class AdminCommands : TdHandler() {
 
     companion object {
 
         const val COMMAND_GC = "gc"
+        const val REVOKE_ALL = "revoke_all"
 
     }
 
     override fun onLoad() {
 
-        initFunction(COMMAND_GC)
+        initFunction(COMMAND_GC, REVOKE_ALL)
 
     }
 
@@ -43,6 +45,17 @@ class AdminCommands : TdHandler() {
                 sudo make "GC Finished" editTo status
 
             }
+
+        } else if (function == REVOKE_ALL) {
+
+            val instances = global.instanceMap.values.toList()
+
+            for (bot in instances) bot.stop()
+            for (bot in instances) bot.waitForClose()
+            for (bot in instances) File(bot.options.databaseDirectory).listFiles()?.forEach {
+                if (it.isFile && (it.extension == "binlog" || it.startsWith("db."))) it.delete()
+            }
+            for (bot in instances) global.initBot(bot.userBot)
 
         }
 
